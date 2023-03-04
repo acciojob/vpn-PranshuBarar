8a0f5676b0cfcabca24d2d79af1d2c99c86e4aa5
91894@PRANSHU <pranshubarar1851996@gmail.com>
2023-03-04 14:23:46 +0530

    First Commit

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,11 +1,19 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+//import java.awt.*;
+import java.awt.*;
+import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -20,13 +28,51 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
+        Admin admin = new Admin();
+        admin.setUsername(username);
+        admin.setPassword(password);
+        return adminRepository1.save(admin);
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+        Admin admin = adminRepository1.findById(adminId).get();
+        ServiceProvider serviceProvider = new ServiceProvider();
+        serviceProvider.setName(providerName);
+        serviceProvider.setAdmin(admin);
+        return adminRepository1.save(admin);
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+        ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+        List<Country> countryList = serviceProvider.getCountryList();
+
+        Country country = new Country();
+
+        if(!caseIgnoreCheckAndEnumCheck(countryName)){
+            throw new Exception("Country not found");
+        }
+
+        country.setCountryName(CountryName.valueOf(countryName));
+//        try {
+//            country.setCountryName(CountryName.caseIgnoreCheck(countryName));
+//        } catch (Exception e){
+//            throw new Exception("Country not found");
+//        }
+
+        country.setCode(CountryName.valueOf(countryName).toCode());
+        countryList.add(country);
+
+        return serviceProviderRepository1.save(serviceProvider);
+    }
+
+    public boolean caseIgnoreCheckAndEnumCheck(String countryName){
+        for (CountryName countryName1 : CountryName.values()) {
+            if (countryName1.name().equalsIgnoreCase(countryName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -21,14 +21,103 @@ public class ConnectionServiceImpl implements ConnectionService {
 
     @Override
     public User connect(int userId, String countryName) throws Exception{
+        User user = userRepository2.findById(userId).get();
+        if(user.isConnected()){
+            throw new Exception("Already connected");
+        }
+        String countryOfUser = user.getCountry().getCountryName().toString();
+        if(countryOfUser.equalsIgnoreCase(countryName)){
+            return user;
+        }
 
+        if(user.getServiceProviderList().isEmpty()){
+            throw new Exception("Unable to connect");
+        }
+        List<ServiceProvider> serviceProviderListOfUser = user.getServiceProviderList();
+        boolean anyOneOfTheServiceProviderHasThisCountry = false;
+        String nameOfServiceProviderServingThisCountry = "";
+        Country countryProviderServes = null;
+        int idOfThisServiceProvider = Integer.MAX_VALUE;
+        for(ServiceProvider serviceProvider : serviceProviderListOfUser){
+            for(Country countryThisProviderServes : serviceProvider.getCountryList()){
+                if(countryThisProviderServes.getCountryName().toString().equalsIgnoreCase(countryName) && serviceProvider.getId()<idOfThisServiceProvider){
+                    if(!anyOneOfTheServiceProviderHasThisCountry){
+                        anyOneOfTheServiceProviderHasThisCountry = true;
+                    }
+                    countryProviderServes = countryThisProviderServes;
+                    nameOfServiceProviderServingThisCountry = serviceProvider.getName();
+                    idOfThisServiceProvider = serviceProvider.getId();
+                }
+            }
+        }
+        if(!anyOneOfTheServiceProviderHasThisCountry){
+            throw new Exception("Unable to connect");
+        }
+        user.setCountry(countryProviderServes);
+
+        user.setMaskedIp(CountryName.valueOf(countryName).toCode()+"."+nameOfServiceProviderServingThisCountry+"."+userId);
+        return userRepository2.save(user);
     }
     @Override
     public User disconnect(int userId) throws Exception {
 
+        User user = userRepository2.findById(userId).get();
+        if(!user.isConnected()){
+            throw new Exception("Already disconnected");
+        }
+
+        user.setConnected(false);
+        user.setMaskedIp(null);
+
+        List<ServiceProvider> serviceProviderListToWhomUserIsConnectedTo = user.getServiceProviderList();
+        for(ServiceProvider serviceProvider : serviceProviderListToWhomUserIsConnectedTo){
+            List<User> userList = serviceProvider.getUsersList();
+            userList.removeIf(user1 -> user1.getId() == userId);
+//            Iterator<User> iterator = userList.iterator();
+//            while(iterator.hasNext()){
+//                if(iterator.next().getId() == userId){
+//                    iterator.remove();
+//                }
+//            }
+        }
+
+
+        List<Connection> connectionListOfThisUser = user.getConnectionList();
+        connectionListOfThisUser.clear();
+
+        return userRepository2.save(user);
     }
     @Override
     public User communicate(int senderId, int receiverId) throws Exception {
+
+        User sender = userRepository2.findById(senderId).get();
+        User receiver = userRepository2.findById(receiverId).get();
+
+        Country currCountryOfReceiver = receiver.getCountry();
+        Country countryOfSender  = sender.getCountry();
+
+        if(!currCountryOfReceiver.getCode().equalsIgnoreCase(countryOfSender.getCode())){
+            //Sender is not connected this time to any vpn
+            List<ServiceProvider> listOfServiceProviderReceiverIsConnectedTo = receiver.getServiceProviderList();
+            if(listOfServiceProviderReceiverIsConnectedTo.isEmpty()){
+                throw new Exception("Cannot establish communication");
+            }
+            int minServiceProviderId = Integer.MAX_VALUE;
+            Country countrySenderIsToBeConnected = null;
+            for(ServiceProvider serviceProvider : listOfServiceProviderReceiverIsConnectedTo){
+                if(serviceProvider.getId()<minServiceProviderId){
+                    countrySenderIsToBeConnected = serviceProvider.getCountryList().get(0);
+                }
+            }
+            sender.setConnected(true);
+            sender.setCountry(countrySenderIsToBeConnected);
+            userRepository2.save(sender);
+        }
+        else {
+            return sender;
+        }
+
+        throw new Exception("Cannot establish communication");
 
     }
 }
