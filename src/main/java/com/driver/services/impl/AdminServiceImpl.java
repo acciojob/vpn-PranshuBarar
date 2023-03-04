6b2c68863ca2e73c4798ab94af8dccf841788e3d
91894@PRANSHU <pranshubarar1851996@gmail.com>
2023-03-04 15:52:10 +0530

    Solution 2

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -70,9 +70,6 @@ public class AdminServiceImpl implements AdminService {
 
     public boolean caseIgnoreCheckAndEnumCheck(String countryName){
         for (CountryName countryName1 : CountryName.values()) {
-            if(!countryName.chars().allMatch(Character::isLowerCase) && !countryName.chars().allMatch(Character::isUpperCase)){
-                return false;
-            }
             if (countryName1.name().equalsIgnoreCase(countryName)) {
                 return true;
             }
