41cca36d82221af4834ac178a3c70caa7eb3f1a3
91894@PRANSHU <pranshubarar1851996@gmail.com>
2023-03-05 12:29:42 +0530

    s78

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -65,8 +65,8 @@ public class ConnectionServiceImpl implements ConnectionService {
         Connection connection1 = connectionRepository2.save(connection);
         user.getConnectionList().add(connection1);
         user.setMaskedIp(CountryName.valueOf(countryName.substring(0,3).toUpperCase()).toCode()+"."+idOfThisServiceProvider+"."+userId);
-        userRepository2.save(user);
-        return user;
+        User user1 = userRepository2.save(user);
+        return user1;
 
     }
     @Override
